90487376069f8b3245eba071377a69bae3da98ba
Bouke Dijkstra <boukedijkstra@hotmail.com>
2023-03-19 20:09:24 +0100

    first set up

---
+++ components/test.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <h1>Hallo</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    h1 {
+        font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    }
+}
+</style>
