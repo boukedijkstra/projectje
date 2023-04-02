47c791e9d308c399133c39f5fc2fdeac73c2976e
Bouke Dijkstra <boukedijkstra@hotmail.com>
2023-04-02 20:47:35 +0200

    knmi buienradar

--- components/test.vue
+++ components/test.vue
@@ -1,25 +1,28 @@
 <template>
-    <div class="buienradar">
-        <iframe src="https://gadgets.buienradar.nl/gadget/zoommap/?lat=53.02132&lng=5.78425&overname=2&zoom=8&naam=8542AT&size=2b&voor=1" scrolling=no width=330 height=330 frameborder=no></iframe>
+    <div class="blok">
+        <img src="https://cdn.knmi.nl/knmi/map/general/weather-map.gif" class="sneekermeer" />
     </div>
-    <div>
-        <iframe width="100%" height="400" src="https://embed.windy.com/embed2.html?lat=53.037&lon=5.757&detailLat=53.096&detailLon=5.836&width=375&height=400&zoom=6&level=surface&overlay=wind&product=ecmwf&menu=&message=true&marker=true&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=kt&metricTemp=%C2%B0C&radarRange=-1" frameborder="0"></iframe>
-    </div>
-    <div>
+    <div class="blok">
         <img src="https://weerstation.kws-sneek.nl/daywindlarge.png" class="sneekermeer" />
     </div>
-    <div class="container">
-        <div v-html="test" />
+    <div class="blok">
+        <iframe width="100%" height="390" src="https://embed.windy.com/embed2.html?lat=53.037&lon=5.757&detailLat=53.096&detailLon=5.836&width=375&height=400&zoom=6&level=surface&overlay=wind&product=ecmwf&menu=&message=true&marker=true&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=kt&metricTemp=%C2%B0C&radarRange=-1" frameborder="0"></iframe>
+    </div>
+    <div class="blok container">
+        <div v-html="knmi" />
     </div>
 </template>
 
 <script setup lang="ts">
 
-const test = await $fetch('/api/knmi');
+const knmi = await $fetch('/api/knmi');
 
 </script>
 
 <style lang="scss" scoped>
+.blok {
+    margin-bottom: 20px;
+}
 .container {
     margin: 0 10px;   
 }
--- server/api/knmi.js
+++ server/api/knmi.js
@@ -1,9 +1,11 @@
 export default defineEventHandler(async (event) => {
     try {
         const response = await $fetch('https://www.knmi.nl/nederland-nu/weer/verwachtingen')
-        const tekst = response.split('<strong>Vanavond</strong>')[1]
-        const tekst2 = tekst.split('<p>(Bron: KNMI)')[0];
-        return `<p><strong>Vanavond</strong> ${tekst2}`
+        const tekst = response.split('<h2>Vandaag')[1]
+        const tekst2 = tekst.split('(Bron: KNMI')[0];
+        const tekst3 = `<h2>Vandaag${tekst2}</p>`
+        console.log(tekst3);
+        return tekst3
     } catch(e) {
         console.log('ERRORRR',e);
     }
